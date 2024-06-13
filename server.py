f645fabc3b6fb50c1e3b90956b3c6d6219a697f5
andrei-mihaicanuta <andrei.canuta@metro.digital>
2024-06-13 14:08:04 +0300

    Created API call for translating NL to SQL syntax

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route('/get_syntax', methods=['POST'])
 def get_syntax():
-    text_query = request.json['text_query']
-    return jsonify(translated_query=sql_syntaxer.translate(text_query)), 200
+    text_query = request.json['text']
+    return jsonify(text=sql_syntaxer.translate(text_query)), 200
 
 
 if __name__ == '__main__':
